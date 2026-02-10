c4207e0c420e7604d7733afc90367f5276d8707d
Bansiraval20 <bansiraval40337@gmail.com>
2026-02-10 18:19:42 +0530

    feat: implement user registration and database connection

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,15 @@
+import { Connection } from "mongoose"
+
 declare module '*.css'
 declare module '*.scss'
 declare module '*.sass'
+
+
+declare global{
+    var mongoose: {
+        conn: Connection | null
+        promise: Promise<Connection> | null
+    }
+}
+
+export {}
